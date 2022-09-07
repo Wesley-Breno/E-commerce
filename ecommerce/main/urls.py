195a069d151bb7968f96158fb52fef362d3c06f9
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2022-09-06 22:34:33 -0300

    Finalizando o projeto
    Adicionei as ultimas coisas que faltavam no projeto ecommerce da aula de Django.

--- ecommerce/main/urls.py
+++ ecommerce/main/urls.py
@@ -1,7 +1,7 @@
-"""main URL Configuration
+"""loja URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -24,9 +24,3 @@ urlpatterns = [
     path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls))
-    ] + urlpatterns
